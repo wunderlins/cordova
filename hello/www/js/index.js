54e9f592b8870d749193fec2b5b1731eee373f5b
Simon Wunderlin <swunderlin@gmail.com>
2016-12-17 11:06:56 +0100

    added bat status

--- hello/www/js/index.js
+++ hello/www/js/index.js
@@ -27,11 +27,23 @@ var app = {
     // Bind any cordova events here. Common events are:
     // 'pause', 'resume', etc.
     onDeviceReady: function() {
-        this.receivedEvent('deviceready');
-        document.getElementById("clickMe").addEventListener("click", this.clickMe, false);
-        //console.log(r)
-    },
-
+			this.receivedEvent('deviceready');
+			document.getElementById("btn-device").addEventListener("click", this.btnClick, false);
+			document.getElementById("btn-battery").addEventListener("click", this.btnClick, false);
+			//console.log(r)
+			this.get_device()
+			document.getElementById("deck-device").style.display = "block";
+			
+			document.addEventListener("pause", this.onPause, false);
+			document.addEventListener("resume", this.onResume, false);
+			document.addEventListener("menubutton", this.onMenuKeyDown, false);
+			document.getElementById("body").addEventListener("load", app.onload, false);
+			
+			window.addEventListener("batterystatus", this.onBatteryStatus, false);
+			window.addEventListener("batterylow", this.onBatteryStatusLow, false);
+			window.addEventListener("batterycritical", this.onBatteryStatusCritical, false);
+		},
+		
     // Update DOM on a Received Event
     receivedEvent: function(id) {
         var parentElement = document.getElementById(id);
@@ -44,22 +56,69 @@ var app = {
         console.log('Received Event: ' + id);
     },
     
+    btnClick: function(e) {
+    	//console.log(e.srcElement.id);
+    	
+    	var id = e.srcElement.id.substr(4);
+    	document.getElementById("deck-device").style.display = "none";
+    	document.getElementById("deck-battery").style.display = "none";
+    	
+    	document.getElementById("deck-" + id).style.display = "block";
+    },
+    
+    // == device information ==
     innerHtml: function(id, markup) {
     	document.getElementById(id).innerHTML = markup;
     },
     
-    clickMe: function(e) {
+    get_device: function(e) {
     	//console.log(e)
-    	app.innerHtml("device-cordova", device.cordova);
-    	app.innerHtml("device-model", device.model);
-    	app.innerHtml("device-platform", device.platform);
-    	app.innerHtml("device-uuid", device.uuid);
-    	app.innerHtml("device-version", device.version);
-    	app.innerHtml("device-manufacturer", device.manufacturer);
-    	app.innerHtml("device-isVirtual", device.isVirtual);
-    	app.innerHtml("device-serial", device.serial);
-    }
+    	this.innerHtml("device-cordova", device.cordova);
+    	this.innerHtml("device-model", device.model);
+    	this.innerHtml("device-platform", device.platform);
+    	this.innerHtml("device-uuid", device.uuid);
+    	this.innerHtml("device-version", device.version);
+    	this.innerHtml("device-manufacturer", device.manufacturer);
+    	this.innerHtml("device-isVirtual", device.isVirtual);
+    	this.innerHtml("device-serial", device.serial);
+    },
     
+    // == battery status ==
+		battery_status: function(status) {
+    	app.innerHtml("battery-level", status.level);
+    	app.innerHtml("battery-isPlugged", status.isPlugged);
+			//console.log("Level: " + status.level + " isPlugged: " + status.isPlugged);
+		},
+
+		onBatteryStatus: function(status) {
+			app.battery_status(status);
+		},
+		
+		onBatteryStatusLow: function(status) {
+			alert("Battery Low");
+			app.battery_status(status);
+		},
+		
+		onBatteryStatusCritical: function(status) {
+			alert("Battery Critical");
+			app.battery_status(status);
+		},
+		
+		// == general events ==
+		onPause: function(e) {
+			// Handle the pause event
+			console.log(e)
+		},
+
+		onResume: function(e) {
+			// Handle the resume event
+			console.log(e)
+		},
+
+		onMenuKeyDown: function(e) {
+			// Handle the menubutton event
+			console.log(e)
+		},
 };
 
 app.initialize();
